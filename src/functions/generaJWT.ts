d75be585fc5be2cf36ba480e51ae7e2fdbaff9fd
greenlaz <erilazerilaz@gmail.com>
2025-01-09 19:01:46 -0300

    final changes2

--- src/functions/generaJWT.ts
+++
@@ -1,6 +0,0 @@
-import { jwt } from "../utils"
-import _secret from "../keys/keys"
-const secret : any  = _secret
-const generaJWT = ( code : any )=> jwt.sign( { code } , secret , { expiresIn:"1h" } ) 
-
-export default generaJWT
--- src/interfaces/blog.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface blog {
-
-    title : string  ,
-    description : string ,
-    images : { image : string}[] ,
-    sections :{ subtitle : string , description : string }[] ,
-    
-}
